5f4c7bc7eafc08f527780aaa9dcc57d3687fa88a
AugustoTaborda <129558169+AugustoTaborda@users.noreply.github.com>
2024-08-26 21:06:31 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import StackNavigator from './src/navigation/StackNavigator';
-
+//import StackNavigator from './src/navigation/StackNavigator';
+//import DrawerNavigation from "./src/navigation/manipulador_nativo";
+//import MyTabs from "./src/navigation/manipulador_button";
+import MyMaterialtopTabs from "./src/navigation/manipulador_MaterialTopTabs";
 
 export default function App() {
-  return ( <StackNavigator />);
+  return (< MyMaterialtopTabs />
+  );
+
 }
